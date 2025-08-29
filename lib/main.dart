1cf4bed603d20508af064565edc8b607472e0cb2
j-lee <j-lee@tisol.co.jp>
2025-08-29 19:40:29 +0900

    feat: Firebase 初期化管理機能追加およびアプリスタート時widget binding初期化

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/app.dart';
 
 void main() async {
+  // Flutter 위젯 바인딩 초기화
+  WidgetsFlutterBinding.ensureInitialized();
+  
   // 앱 부트스트랩 초기화 (환경변수 로드 포함)
   await AppBootstrap.initialize();
   
