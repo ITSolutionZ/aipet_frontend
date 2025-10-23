48003fef783a2b2c4a7ba8217982eb97e731864f
j-lee <j-lee@tisol.co.jp>
2025-10-23 12:21:04 +0900

    fix: base 클래스 Result → ResultState 일괄 변경 (112개 에러 해결)

--- lib/features/pet_feeding/data/repositories/pet_feeding_repository_impl.dart
+++ lib/features/pet_feeding/data/repositories/pet_feeding_repository_impl.dart
@@ -1,9 +1,8 @@
 import 'package:aipet_frontend/features/pet_feeding/data/models/feeding_record_model.dart';
 import 'package:aipet_frontend/features/pet_feeding/data/services/pet_feeding_local_storage_service.dart';
 import 'package:aipet_frontend/features/pet_feeding/domain/entities/feeding_record_entity.dart';
-import 'package:aipet_frontend/shared/core/utils/date_time_utils.dart';
-import 'package:aipet_frontend/shared/core/utils/date_time_utils.dart';
 import 'package:aipet_frontend/features/pet_feeding/domain/repositories/pet_feeding_repository.dart';
+import 'package:aipet_frontend/shared/core/utils/date_time_utils.dart';
 
 class PetFeedingRepositoryImpl implements PetFeedingRepository {
   PetFeedingRepositoryImpl();
